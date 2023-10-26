d451b7492609afcc1c5ec6d26086d489fdd9ac43
SkyClave <alwindjuliansah07@gmail.com>
2023-10-26 23:59:42 +0700

    halaman pinjam done

--- pinjam_buku/urls.py
+++ pinjam_buku/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from pinjam_buku.views import pinjam_buku_outer
 
 app_name = 'pinjam_buku'
 
 urlpatterns = [
-    
+    path('<int:id>/', pinjam_buku_outer, name='pinjam_buku'),
 ]
--- pinjam_buku/views.py
+++ pinjam_buku/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from daftar_buku.models import Buku
 
 # Create your views here.
+def pinjam_buku_outer(request, id):
+    buku = Buku.objects.filter(pk=id).first()
+    context = {
+        'buku': buku,
+    }
+
+    return render(request, "pinjambuku.html", context)
