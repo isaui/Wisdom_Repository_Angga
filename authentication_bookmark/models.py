7232f113ae3322f0e44c60af3f44b2324511a579
Anisha Putri Qonitah <nishaputriq@gmail.com>
2023-10-28 16:37:16 +0700

    feat: bookmark attempt 1

--- authentication_bookmark/models.py
+++ authentication_bookmark/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import Group, Permission
 from django.db import models
+from daftar_buku.models import Buku
 
 class CustomUser(AbstractUser):
     member = models.CharField(
@@ -15,8 +16,6 @@ class CustomUser(AbstractUser):
     groups = models.ManyToManyField(Group, related_name='custom_users_groups')
     user_permissions = models.ManyToManyField(Permission, related_name='custom_users_permissions')
 
-
 class Bookmark(models.Model):
-    buku = models.CharField(max_length=255)
-    gambar = models.CharField(max_length=100)
+    buku = models.ForeignKey(Buku, on_delete=models.CASCADE)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
--- authentication_bookmark/views.py
+++ authentication_bookmark/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+import json
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
-from django.shortcuts import redirect
 from django.contrib import messages  
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
@@ -13,16 +13,16 @@ from .forms import SignupForm
 from django.views.decorators.csrf import csrf_exempt
 from authentication_bookmark.models import Bookmark
 from django.core import serializers
+from daftar_buku.models import Buku
 
 # Create your views here.
 @login_required(login_url='/login')
 def show_bookmark(request):
-    bookmark = Bookmark.objects.filter(user=request.user)    
+    bookmarks = Bookmark.objects.filter(user=request.user)    
     context = {
         'name': request.user.username,
         'member' : request.user.member,
-        'bookmark': bookmark,
-        # 'last_login': request.COOKIES['last_login'],
+        'bookmarks': bookmarks,
     }
 
     return render(request, "bookmark.html", context)
@@ -46,7 +46,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            response = HttpResponseRedirect(reverse('authentication_bookmark:show_bookmark')) 
+            response = HttpResponseRedirect(reverse('daftar_buku:show_main')) 
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
         else:
@@ -60,21 +60,51 @@ def logout_user(request):
 
 @csrf_exempt
 def add_bookmark_ajax(request):
-    if request.method == 'POST':
-        buku = request.POST.get("buku")
-        gambar = request.POST.get("gambar")
+    if request.method == 'GET':
+        id_buku = request.GET.get('id_buku')
+        buku = Buku.objects.get(pk=id_buku)
         user = request.user
 
-        new_bookmark = Bookmark(buku=buku, gambar=gambar, user=user)
+        new_bookmark = Bookmark(buku=buku, user=user)
         new_bookmark.save()
+        return JsonResponse({'message': 'Bookmark berhasil ditambahkan'}, status=201)
 
-        return HttpResponse(b"CREATED", status=201)
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            id_buku = data.get('idbuku')  # Pastikan kunci dalam data adalah 'idbuku'
+            buku = Buku.objects.get(pk=id_buku)
+            user = request.user
 
-    return HttpResponseNotFound()
+            new_bookmark = Bookmark(buku=buku, user=user)
+            new_bookmark.save()
 
+            return JsonResponse({'message': 'Bookmark berhasil ditambahkan'}, status=201)
+        except Buku.DoesNotExist:
+            return JsonResponse({'message': 'ID buku tidak ditemukan'}, status=400)
+        except Exception as e:
+            return JsonResponse({'message': f'Gagal menambahkan bookmark: {str(e)}'}, status=400)
+
+    return JsonResponse({'message': 'Metode tidak diizinkan'}, status=405)
+
+# def add_bookmark_ajax(request, id):
+#     # Get data berdasarkan ID
+#     buku = Buku.objects.get(pk = id)
+#     user = request.user
+#     # Hapus data
+#     new_bookmark = Bookmark(buku=buku, user=user)
+#     new_bookmark.save()
+
+#     return HttpResponse(b"CREATED", status=201)
+  
+ 
 def get_bookmark_json(request):
     bookmark_item = Bookmark.objects.all()
     return HttpResponse(serializers.serialize('json', bookmark_item))
+
+def show_json(request):
+    data = Bookmark.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def delete_bookmark(request, id):
     # Get data berdasarkan ID
