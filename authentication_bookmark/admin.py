1a7b76c9b245351c61562ed89e616ec28d03edaf
Anisha Putri Qonitah <nishaputriq@gmail.com>
2023-10-29 22:19:45 +0700

    fix: admin

--- authentication_bookmark/admin.py
+++ authentication_bookmark/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from .models import CustomUser, Bookmark
 
-admin.site.register(CustomUser)
-admin.site.register(Bookmark)
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'member', 'password')
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('buku', 'user', 'judul', 'gambar')
